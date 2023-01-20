bfb9394c83de9e1651fbdf8f8fc4dcdca97732f0
Donghui Go <85717356+dongdong-119@users.noreply.github.com>
2023-01-20 10:01:35 +0900

    Update main.py

--- main.py
+++ main.py
@@ -5,11 +5,6 @@ RSS_FEED = feedparser.parse(URL)
 MAX_POST = 5
 
 markdown_text = """
-### :hand: Hi, this is Donghui's Github<br/>
-hi, my name is Donghui, and I a am web back-end developer. I'm interested in not only web and database technologies but policy environment surroungding them.
-
-<br/><br/><br/>
-
 #### :hammer_and_wrench: Projects<br/>
 - [국회의원 발의 법률안 통계 사이트](https://github.com/dongdong-119/StatisticalDataOnLegislation)
 - [향수 어플리케이션(개발중)](https://github.com/kimyounil1/AllPouse)
